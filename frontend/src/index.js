be58173314236197331aa93edb3deb62843f04e9
Jorge <jbocanegragrandez@gmail.com>
2018-10-22 21:34:42 -0700

    Add Frontend structure

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as APIUtil from "./util/session_api_util";
+import jwt_decode from "jwt-decode";
+import configureStore from "./store/store";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+document.addEventListener('DOMContentLoaded', () => {
+    let store = configureStore();
+    // Check for token
+    if (localStorage.jwtToken) {
+        // Set auth token header auth
+        APIUtil.setAuthToken(localStorage.jwtToken);
+        // Decode token and get user info and exp
+        const decoded = jwt_decode(localStorage.jwtToken);
+        // Set user and isAuthenticated
+        store.dispatch(APIUtil.setCurrentUser(decoded));
+
+        // Check for expired token
+        const currentTime = Date.now() / 1000;
+        if (decoded.exp < currentTime) {
+            // Logout user
+            store.dispatch(APIUtil.logoutUser());
+            // Redirect to login
+            window.location.href = '/login';
+        }
+    }
+
+
+ReactDOM.render(<App store={store}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+});
