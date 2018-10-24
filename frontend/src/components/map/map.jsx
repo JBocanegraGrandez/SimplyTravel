a0f1f13d9a44a922f90b2156c97f4b6246831068
Alejandro Chang <alejandrochang10@gmail.com>
2018-10-23 19:47:37 -0700

    Organizatin of map component

--- frontend/src/components/map/map.jsx
+++ frontend/src/components/map/map.jsx
@@ -2,23 +2,30 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
+const style = {
+  width: '100%',
+  height: '100%'
+}
+
+
 class MapContainer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
   render() {
     return (
-      <div className="map">
-        <Map google={this.props.google} zoom={4}>
-
-          <Marker onClick={this.onMarkerClick}
-            name={'Current location'} />
-
-          <InfoWindow onClose={this.onInfoWindowClose}>
-            <div>
-            </div>
+      <div className="map" ref="map">
+        <Map google={this.props.google} style={style} zoom={10} onClick={this.MapClicked}>
+          <Marker onClick={this.onMarkerClick} name={"Current location"} />
+          <InfoWindow 
+          onClose={this.onInfoWindowClose}>
+          <div>
+          </div>
           </InfoWindow>
         </Map>
       </div>
-    )
+    );
   }
 }
 
--- frontend/src/components/root.jsx
+++ frontend/src/components/root.jsx
@@ -12,14 +12,13 @@ import MapContainer from './map/map';
 const Root = () => {
  return(
   <div>
-   <header>
-    <Link to="/" className="home-link">
-     <h1>SimplyTravel</h1>
-    </Link>
-   </header>
-   <Switch>
-     <Route path="/map" component={MapContainer}/>
-   </Switch>
+    <header>
+     <Link to="/" className="home-link">
+     </Link>
+    </header>
+    <Switch>
+      <Route path="/map" component={MapContainer}/>
+    </Switch>
   </div>
  )
 }
