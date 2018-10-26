3f3916d405e5ca1fe10f7fe13a164dcce8aeca90
Henry Chu <chu.henry@hotmail.com>
2018-10-26 11:36:43 -0700

    hotels container push

--- frontend/src/components/hotels/hotels.jsx
+++ frontend/src/components/hotels/hotels.jsx
@@ -1,27 +1,94 @@
 import React from 'react';
-const axios = require('axios');
+import axios from 'axios';
 
 class Hotels extends React.Component {
   constructor() {
     super()
     this.state = {
-      info: '',
+      data: [],
     }
   }
 
-  testMe() {
-    axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=hotel&key=AIzaSyBQzt2BjKglqfdpP4fb1d5beRvYK0A45ac`)
-      .then(response => console.log(response))
+  testMe = () => {
+    axios
+      .get(
+        `http://api.sandbox.amadeus.com/v1.2/hotels/search-circle?latitude=43.6&longitude=7.2&radius=50&check_in=2018-11-01&check_out=2018-11-03&number_of_results=50&apikey=AvrAMRvVBrXJA89mKwfYTtRWkufuwAZI`
+      )
+      .then((response) => {
+        this.setState({ data: response.data.results });
+      })
   }
 
+  // either do it inside .then or change another .then to manipulate data
+  // could also use setState if youre trying to update component
+  // promise.resolve to send data out of the asynchronous call
+
+  createStars = stars => { }
+
   render() {
+    let starClass;
+    let stars = Math.round(this.state.rating);
+
+    starClass = this.state.rating > 1 ? "real-stars" : "no-stars"
+
     return <div>
       test meeee again
-        <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBQzt2BjKglqfdpP4fb1d5beRvYK0A45ac&libraries=places" />
-      <form onSubmit={this.testMe}>
-        <input type="submit" value="Submit" />
-      </form>
-    </div>;
+        <div className="hotels">
+        <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBQzt2BjKglqfdpP4fb1d5beRvYK0A45ac&libraries=places"></script>
+        <form onSubmit={this.testMe}>
+          <input type="submit" value="Submit" />
+        </form>
+        <div className="hotels-container">
+          {this.state.data.map((el) => (
+            <div className="individual-selection">
+              <div className="hotel-stats-container">
+                <div className="hotel-name-price-container">
+                  <span className="hotel-name">
+                    {el.property_name}
+                  </span>
+                  <span className="hotel-price">
+                    {el.total_price.amount} USD
+                    </span>
+                </div>
+                <div className="hotel-rating">
+
+                </div>
+                <div className="hotel-address">
+                  {el.address.line1} <br />
+                  {el.address.city},&nbsp;
+                  {el.address.postal_code}&nbsp;
+                  {el.address.country}
+                </div>
+                <div className="marketing-text">
+                  {el.marketing_text}
+                  <br />
+                  {el.amenities.map(ele => {
+                    if (ele.amenity.toLowerCase().includes("internet")) {
+                      return (<div>render something</div>)
+                    }
+                  })}
+                </div>
+                <div className="image-container">
+                  {
+                    (el.awards[0] === undefined) ? (
+                      (stars = 0) || null
+                    ) : (
+                        (stars = el.awards[0].rating) && null
+                      )
+                  }
+
+
+
+                </div>
+
+                <div className="testing">{stars}</div>
+
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   }
 }
 
@@ -38,8 +105,13 @@ export default Hotels;
 //     })
 // }
 
-// handleClick(){
-//   axios.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=hotel&key=AIzaSyBQzt2BjKglqfdpP4fb1d5beRvYK0A45ac")
-//     .then(response => 
-//         const locations = response.data.results;
-//       this.setState({ locations })
+
+// QUESTIONS:
+// what is loading: true and loading: false
+// will we have rendering problem if we pull the hotels data from componentDidMount rather than passing it down from the state
+// if no rendering problem, how can we let user favorite/save chosen hotels?
+// where are the hotel prices?
+
+
+// amadeus key 
+// AvrAMRvVBrXJA89mKwfYTtRWkufuwAZI
---
+++ frontend/src/components/hotels/hotels_container.js
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux';
+import Hotels from './hotels';
+
+// const mapStateToProps = (statem ownProps) => {
+//   return ({
+//     lat:
+//       lng:
+//     check_in:
+//       check_out:
+//   })
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Hotels);
