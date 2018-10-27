3b5d711d1efc5ed32441136586d37110afb22ac1
Alejandro Chang <alejandrochang10@gmail.com>
2018-10-27 16:18:35 -0700

    home bar added

--- frontend/src/components/home/index.jsx
+++ frontend/src/components/home/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import LocationSearchInput from '../location_search_bar/location_search_input';
 
 export default function index() {
   return (
-    <div className="modal-background">
-      
+    <div>
+      <div className="modal-background"></div>
+      <div className="home-search-bar-container">
+        <LocationSearchInput />
+      </div>
     </div>
   )
 }
