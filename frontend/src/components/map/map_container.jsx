d72d6e65465f8b63abd7a40c5e6f54a97ba0124e
Alejandro Chang <alejandrochang10@gmail.com>
2018-10-28 17:29:36 -0700

    updated map container

--- frontend/src/components/map/map_container.jsx
+++ frontend/src/components/map/map_container.jsx
@@ -1,17 +1,23 @@
 import { connect} from 'react-redux';
 import Map from './map';
+import { pinLocation } from '../../actions/location_actions';
+import { pinDestination } from '../../actions/destination_actions';
 
 const mapStateToProps = state => {
   // the state of our events, places etc. that we want to pass through
   return {
-
+    location: state.entities.location, // obj of lat and lng
+    destination: state.entities.destination
   };
 }
 
 const mapDispatchToProps = dispatch => {
   // anything we want to dispatch to our map component
   return  {
-
+    pinLocation: (location) => dispatch(pinLocation(location)),
+    // connect geocode user location to pinLocation
+    pinDestination: (destination) => dispatch(pinDestination(destination))
+    // invoke pin destination on pinDestination
   };
 }
 
