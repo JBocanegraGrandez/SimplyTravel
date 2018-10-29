cb4250b0718aa16988980dd608149f8123b84a1f
john <johnhuang121@gmail.com>
2018-10-28 18:56:32 -0700

    delete gitignore build in frontend

---
+++ frontend/build/precache-manifest.258a77e0e7aaf6b1635bbb570c000169.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fc2ca65528c7f8ca2301682aae97c43",
+    "url": "/static/media/5.8fc2ca65.jpg"
+  },
+  {
+    "revision": "836c40485e94a482ff5b",
+    "url": "/static/css/main.7eaa1805.chunk.css"
+  },
+  {
+    "revision": "581989818fb67ad5dc8e",
+    "url": "/static/js/1.58198981.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "134b4ab619a8ba96be1e89956e493176",
+    "url": "/static/media/1.134b4ab6.jpg"
+  },
+  {
+    "revision": "ec8ac752e60f24a308d0ff292b32d523",
+    "url": "/static/media/2.ec8ac752.jpg"
+  },
+  {
+    "revision": "c2aaf7788339d12acb673ae9709d36aa",
+    "url": "/static/media/3.c2aaf778.jpg"
+  },
+  {
+    "revision": "86d4e90382a7ab17f28cfeb7adbf2221",
+    "url": "/static/media/4.86d4e903.jpg"
+  },
+  {
+    "revision": "836c40485e94a482ff5b",
+    "url": "/static/js/main.836c4048.chunk.js"
+  },
+  {
+    "revision": "52135f9b7d7d61135deda6ee03a4608b",
+    "url": "/static/media/6.52135f9b.jpg"
+  },
+  {
+    "revision": "025f3964bada391df3f3cbdb56cb9ee5",
+    "url": "/static/media/7.025f3964.jpg"
+  },
+  {
+    "revision": "8a80d00ddabc241408aad9796ac5299d",
+    "url": "/static/media/8.8a80d00d.jpg"
+  },
+  {
+    "revision": "93c1efb70ab248fb60ebfa288d613aca",
+    "url": "/static/media/9.93c1efb7.jpg"
+  },
+  {
+    "revision": "8641a5d5a517d75590f6bfe1709c266b",
+    "url": "/static/media/10.8641a5d5.jpg"
+  },
+  {
+    "revision": "c995ae9de9131f5adbe038081b0a0f5f",
+    "url": "/static/media/11.c995ae9d.jpg"
+  },
+  {
+    "revision": "855310d9309d3908fc598694d7101d77",
+    "url": "/static/media/collage.855310d9.jpg"
+  },
+  {
+    "revision": "c28d0b7dbe5e039a18def68f98309e8c",
+    "url": "/index.html"
+  }
+];
