78004cc4e7742584f1e3faafeb37163f83493f2a
Alejandro <alejandrochang10@gmail.com>
2019-01-12 19:35:18 -0800

    updating colors to icons for about us page

--- frontend/src/components/about_us/about_us.js
+++ frontend/src/components/about_us/about_us.js
@@ -18,51 +18,51 @@ const aboutUs = () => {
       <section className="section2">
         <h2 className="team">Meet the Team!</h2>
         <div className="gallery-container">
-            <div className="gallery">
-              <img src="https://s3-us-west-2.amazonaws.com/aa-progress-tracker/students/avatars/000/002/974/medium/Alejandro_Chang.jpg?1534882493" alt="alejandro"/>
-              <div className="desc">Alejandro Chang</div>
-              <div className="desc-2">Software Engineer</div>
-              <div className="logos">
-                <i class="globe icon"></i>
-                <i class="google plus g icon"></i >
-                <i class="github icon"></i>
-                <i class="linkedin icon"></i>
-              </div>
-            </div>
-            <div className="gallery">
-              <img src="https://s3-us-west-2.amazonaws.com/aa-progress-tracker/students/avatars/000/002/975/medium/John_Huang_2.jpg?1534886983" alt="john" style={{ height: '252px'}}/>
-              <div className="desc">John Huang</div>
-              <div className="desc-2">Software Engineer</div>
-              <div className="logos">
-                <i class="globe icon"></i>
-                <i class="google plus g icon"></i >
-                <i class="github icon"></i>
-                <i class="linkedin icon"></i>
-              </div>
-            </div>
-            <div className="gallery">
-              <img src="https://s3-us-west-2.amazonaws.com/aa-progress-tracker/students/avatars/000/003/022/medium/Jorge_Bocanegra_2.jpg?1534887075" alt="jorge" style={{ height: '252px' }}/>
-              <div className="desc">Jorge Bocanegra</div>
-              <div className="desc-2">Software Engineer</div>
-              <div className="logos">
-                <i class="globe icon"></i>
-                <i class="google plus g icon"></i >
-                <i class="github icon"></i>
-                <i class="linkedin icon"></i>
-              </div>
-            </div>
-            <div className="gallery">
-              <img src="https://s3-us-west-2.amazonaws.com/aa-progress-tracker/students/avatars/000/003/011/medium/Henry_Chu_2.jpg?1534886856" alt="henry" style={{ height: '252px' }}/>
-              <div className="desc">Henry Chu</div>
-              <div className="desc-2">Software Engineer</div>
-              <div className="logos">
-                <i class="globe icon"></i>
-                <i class="google plus g icon"></i >
-                <i class="github icon"></i>
-                <i class="linkedin icon"></i>
-              </div>
+          <div className="gallery">
+            <img src="https://s3-us-west-2.amazonaws.com/aa-progress-tracker/students/avatars/000/002/974/medium/Alejandro_Chang.jpg?1534882493" alt="alejandro" />
+            <div className="desc">Alejandro Chang</div>
+            <div className="desc-2">Software Engineer</div>
+            <div className="logos">
+              <a href="https://www.alejandrochang.com"><i class="globe icon" /></a>
+              <i class="google plus g icon" />
+              <i class="github icon" />
+              <i class="linkedin icon" />
             </div>
           </div>
+          <div className="gallery">
+            <img src="https://s3-us-west-2.amazonaws.com/aa-progress-tracker/students/avatars/000/002/975/medium/John_Huang_2.jpg?1534886983" alt="john" style={{ height: "252px" }} />
+            <div className="desc">John Huang</div>
+            <div className="desc-2">Software Engineer</div>
+            <div className="logos">
+              <i class="globe icon" />
+              <i class="google plus g icon" />
+              <i class="github icon" />
+              <i class="linkedin icon" />
+            </div>
+          </div>
+          <div className="gallery">
+            <img src="https://s3-us-west-2.amazonaws.com/aa-progress-tracker/students/avatars/000/003/022/medium/Jorge_Bocanegra_2.jpg?1534887075" alt="jorge" style={{ height: "252px" }} />
+            <div className="desc">Jorge Bocanegra</div>
+            <div className="desc-2">Software Engineer</div>
+            <div className="logos">
+              <i class="globe icon" />
+              <i class="google plus g icon" />
+              <i class="github icon" />
+              <i class="linkedin icon" />
+            </div>
+          </div>
+          <div className="gallery">
+            <img src="https://s3-us-west-2.amazonaws.com/aa-progress-tracker/students/avatars/000/003/011/medium/Henry_Chu_2.jpg?1534886856" alt="henry" style={{ height: "252px" }} />
+            <div className="desc">Henry Chu</div>
+            <div className="desc-2">Software Engineer</div>
+            <div className="logos">
+              <i class="globe icon" />
+              <i class="google plus g icon" />
+              <i class="github icon" />
+              <i class="linkedin icon" />
+            </div>
+          </div>
+        </div>
       </section>
     </div>;
 };
